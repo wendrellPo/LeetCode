08437f7f0e04ce7c98f739ddbf87f59727c74c4e
wendrellPo <wendrellbr@gmail.com>
2024-08-19 17:00:40 -0300

    Resolvido exercício 1

---
+++ 1.two-sum.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=1 lang=python
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        index = {}
+
+        for i, num in enumerate(nums):
+            aux = target - num
+            if aux in index:
+                return [index[aux], i]
+            index[num] = i
+        
+# @lc code=end
+
