65666f4de27791feb9614e915d38102defb38180
wendrellPo <wendrellbr@gmail.com>
2024-08-22 10:17:55 -0300

    Resolvido exercício 1929

---
+++ 1929.concatenation-of-array.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=1929 lang=python
+#
+# [1929] Concatenation of Array
+#
+
+# @lc code=start
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return nums + nums
+
+# Complexidade de Tempo: O(n)
+# Complexidade de Espaço: O(n)
+        
+# @lc code=end
+
